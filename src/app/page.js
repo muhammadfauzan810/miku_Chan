af4c34765b532eb45b5e66654f83d4a8a892fbaa
muhammadfauzan810 <muhammadfauzan890@gmail.com>
2024-10-31 15:19:15 +0700

    update terbaru 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import AnimeList from "@/components/AnimeList";
+import Link from "next/link";
 const Home = async () => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
     const anime = await response.json();
@@ -6,9 +7,9 @@ const Home = async () => {
         <div className="p-6 bg-gray-100 rounded-lg shadow-md">
             <div className="flex justify-between items-center mb-6 p-4 bg-white rounded-lg shadow">
                 <h1 className="text-3xl font-extrabold text-gray-800">Paling Populer</h1>
-                <a href="/semua-anime" className="px-5 py-2 bg-gradient-to-r from-teal-500 to-teal-600 text-white font-semibold rounded-lg shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
+                <Link href="/semua-anime" className="px-5 py-2 bg-gradient-to-r from-teal-500 to-teal-600 text-white font-semibold rounded-lg shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl">
                     Lihat Semua
-                </a>
+                </Link>
             </div>
             <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-6">
                 {anime.data.map((data) => {
