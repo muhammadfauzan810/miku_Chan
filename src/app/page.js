62fc6af10b4f10628307db5a2a90a1159886808d
muhammadfauzan810 <muhammadfauzan890@gmail.com>
2024-10-31 13:56:02 +0700

    test1

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
+// app/page.js
 import AnimeList from "@/components/AnimeList";
+
 const Home = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`);
-    const anime = await response.json();
+    let anime;
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`, {
+            cache: "no-store", // Pastikan untuk selalu mengambil data terbaru
+        });
+
+        if (!response.ok) {
+            console.error("Failed to fetch data:", response.statusText);
+            // Jika gagal, Anda bisa mengembalikan nilai default atau mengatur anime menjadi array kosong
+            anime = { data: [] };
+        } else {
+            anime = await response.json();
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        anime = { data: [] }; // Atau Anda bisa mengatur ini menjadi null atau nilai default lainnya
+    }
+
     return (
         <div className="p-6 bg-gray-100 rounded-lg shadow-md">
             <div className="flex justify-between items-center mb-6 p-4 bg-white rounded-lg shadow">
@@ -11,13 +30,11 @@ const Home = async () => {
                 </a>
             </div>
             <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-6">
-                {anime.data.map((data) => {
-                    return (
-                        <div key={data.mal_id} className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform transform hover:shadow-xl hover:scale-103">
-                            <AnimeList title={data.title} image={data.images.webp.image_url} id={data.mal_id} />
-                        </div>
-                    );
-                })}
+                {anime.data.map((data) => (
+                    <div key={data.mal_id} className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform transform hover:shadow-xl hover:scale-103">
+                        <AnimeList title={data.title} image={data.images.webp.image_url} id={data.mal_id} />
+                    </div>
+                ))}
             </div>
         </div>
     );
